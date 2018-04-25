e6faa454d378c523a41ef1873d0895803df04b22
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-04-25 13:33:45 +0200

    Create EtatAutomate.java

---
+++ EtatAutomate.java
@@ -0,0 +1,25 @@
+package Classe;
+
+public enum EtatAutomate {
+    E_INIT,
+    E_FIN,
+    E_ENTIER,
+    E_EGAL,
+    E_EXCL,
+    E_INF,
+    E_SUP,
+    E_1I,
+    E_2E,
+    E_2L,
+    E_2S,
+    E_3O,
+    E_3U,
+    E_4P,
+    E_4U,
+    E_4L,
+    E_4S,
+    E_5J,
+    E_5U,
+    E_5M
+}
+
