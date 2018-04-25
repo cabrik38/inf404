7d691bd5adf52a61734f9051d6c111e9314f1c2e
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-04-25 13:32:40 +0200

    Update lexeme.java

--- lexeme.java
+++ lexeme.java
@@ -1,33 +1,7 @@
-public enum Nature_Lexeme{
-  A,
-  B,
-  IF,
-  ELSE,
-  INF,
-  INFE,
-  SUP,
-  SUPE,
-  EGAL,
-  DIFF,
-  AFFECT,
-  PLUS,
-  MOINS,
-  MUL,
-  DIV,
-  PARO,
-  PARF,
-  ACCO,
-  ACCF,
-  IN,
-  OUT,
-  PULL,
-  PUSH,
-  JUMP,
-  ENTIER,
-  ERREUR;
-}
+package Classe;
+
 
 public class Lexeme{
-  private Nature_Lexeme nature;
-  private int valeur; //pour le jump
+    public Nature_Lexeme nature;
+    public int valeur;              // pour le jump
 }
