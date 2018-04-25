581a6fc3eb6be975951a82e0ad88861063410e77
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-04-25 13:33:06 +0200

    Create Nature_Lexeme.java

---
+++ Nature_Lexeme.java
@@ -0,0 +1,31 @@
+package Classe;
+
+public enum Nature_Lexeme {
+    A,
+    B,
+    IF,
+    ELSE,
+    INF,
+    INFE,
+    SUP,
+    SUPE,
+    EGAL,
+    DIFF,
+    AFFECT,
+    PLUS,
+    MOINS,
+    MUL,
+    DIV,
+    PARO,
+    PARF,
+    ACCO,
+    ACCF,
+    IN,
+    OUT,
+    PULL,
+    PUSH,
+    JUMP,
+    ENTIER,
+    FIN_SEQUENCE,
+    ERREUR
+}
