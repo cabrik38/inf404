186f262c5557806ca9e41a283c6a135b5dec4890
madoci <36835658+madoci@users.noreply.github.com>
2018-05-02 17:04:51 +0200

    Create InstructionOUT.java

---
+++ InstructionOUT.java
@@ -0,0 +1,18 @@
+package Classe;
+
+public class InstructionOUT extends Instruction {
+    private booleann _paramA;
+    
+    public InstructionOUT(boolean paramA){
+        _paramA = paramA;
+    }
+    
+    public int executer(Donnees &d){
+        if (paramA){
+            d.pushOut(d.getA());
+        } else {
+            d.pushOut(d.getB());
+        }
+        return 1;
+    }
+}
