a817ce15ba29fa308884da265ed1caf870b0e4a4
madoci <36835658+madoci@users.noreply.github.com>
2018-05-03 03:16:33 +0200

    Update InstructionIF.java

--- InstructionIF.java
+++ InstructionIF.java
@@ -8,13 +8,34 @@ public class InstructionIF extends Instruction {
     private int _pointeur;
     
     public InstructionIF(Condition cond){
-        _sequenceIF = new ArrayList<Instruction>();
-        _sequenceELSE = new ArrayList<Instruction>();
+        _sequenceIF = new SequenceInstructions();
+        _sequenceELSE = new SequenceInstructions();
         _condition = cond;
         _pointeur = 0;
     }
     
     public int executer(Donnees &d){
-        
+        if (_pointeur == 0){
+            _condition.update();
+            _sequenceIF.setPointeur(1);
+            _sequenceELSE.setPointeur(1);
+            _pointeur = 1;
+        } else {
+            if (_condition.estVraie()){
+                int s = _sequenceIF.step();
+            } else {
+                int s = _sequenceELSE.step();
+            }
+            if (s == -1){
+                _pointeur = 0;
+            } else if (s < -1){
+                _pointeur = 0;
+                return s + 1;
+            } else if (s > 0){
+                _pointeur = 0;
+                return s;
+            }
+        }
+        return 0;
     }
 }
