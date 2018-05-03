1d7440056e5b488fbcc8b01175c405328854770b
madoci <36835658+madoci@users.noreply.github.com>
2018-05-03 04:30:48 +0200

    Update Script.java

--- Script.java
+++ Script.java
@@ -1,13 +1,15 @@
 package Classe;
 
 public class Script {
-      private SequenceInstructions _seq;
-      
-      public Script(){}
-      
-      public void step(Donnees &d){
-            if (!d.stopped()){
-                  _seq.setPointeur(_seq.step(d));
-            }
-      }
+    private SequenceInstructions _seq;
+
+    public Script(SequenceInstructions seq){
+        _seq = seq;
+    }
+
+    public void step(Donnees &d){
+        if (!d.stopped()){
+            _seq.setPointeur(_seq.step(d));
+        }
+    }
 }
